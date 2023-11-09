0d321bb6de238df47edd592214f8b3d95cc0dddd
L7z404 <leonelcastillo1014@gmail.com>
2023-11-09 16:21:01 -0600

    Getting info from pokemon api correctly

--- app/src/main/java/com/example/pokemon_api/MainActivity.java
+++ app/src/main/java/com/example/pokemon_api/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.pokemon_api;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView data;
+    String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        data = findViewById(R.id.data);
+        url = "https://pokeapi.co/api/v2/pokemon/gulpin";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String name = response.getString("name");
+                    data.setText(name);
+                }catch ( Exception e){
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        Volley.newRequestQueue(this).add(request);
     }
 }
