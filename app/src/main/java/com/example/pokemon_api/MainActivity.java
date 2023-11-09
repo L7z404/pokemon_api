c8098f774e2a9e3d53e08436b00e579d1cf0f835
L7z404 <leonelcastillo1014@gmail.com>
2023-11-09 17:44:05 -0600

    Added another value from api, constraintlayout, not working with the inner json array/objects

--- app/src/main/java/com/example/pokemon_api/MainActivity.java
+++ app/src/main/java/com/example/pokemon_api/MainActivity.java
@@ -11,12 +11,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
 
     TextView data;
+    TextView expnumber;
     String url;
 
     @Override
@@ -25,14 +27,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         data = findViewById(R.id.data);
+        expnumber = findViewById(R.id.expnumber);
         url = "https://pokeapi.co/api/v2/pokemon/gulpin";
 
+        //Creating a request to pull data from api and assign it to the textview
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
                     String name = response.getString("name");
+                    String exp = response.getString("base_experience");
+                    //String abilityname = "";
+                    //String ability = response.getJSONArray("abilities").getJSONObject(0).toString();
+
+                    //JSONArray abilities = response.getJSONArray("abilities");
+                    //for (int i = 0; i < abilities.length(); i++) {
+                    //    JSONObject ability = abilities.getJSONObject(i);
+                    //    abilityname = ability.getString("name");
+                    //}
                     data.setText(name);
+                    expnumber.setText(exp);
+
+
                 }catch ( Exception e){
 
                 }
