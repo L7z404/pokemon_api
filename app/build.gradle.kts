f6bd571f6fd8acf8577ff7583d8ef643188cd369
L7z404 <leonelcastillo1014@gmail.com>
2023-11-09 16:09:15 -0600

    Added MainActivity with layout, modified manifest... app loads view with text correctly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.pokemon_api"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pokemon_api"
@@ -35,4 +35,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.android.volley:volley:1.2.1")
 }
